bffb6502e54fcb7224071b295d4a07bdcc16f249
MahammadMomin <mohammad.suthar@sparepartwale.com>
2026-01-05 11:05:32 +0530

    feat: Initialize UserAuthApp2 with basic navigation and authentication screens
    - Created the main application component (App.tsx) to set up navigation.
    - Implemented AppNavigator with Login, Signup, and Home screens.
    - Added basic structure for LoginScreen, SignupScreen, and HomeScreen.
    - Defined User and AuthContextType interfaces for user authentication.
    - Set up the project workspace for Xcode.

---
+++ src/navigation/AppNavigator.tsx
@@ -0,0 +1,23 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import LoginScreen from '../screens/LoginScreen';
+import SignupScreen from '../screens/SignupScreen';
+import HomeScreen from '../screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
+
+const AppNavigator = () => {
+  return (
+   <NavigationContainer>
+    <Stack.Navigator initialRouteName="Login">
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Signup" component={SignupScreen} />
+      <Stack.Screen name="Home" component={HomeScreen} />
+    </Stack.Navigator>
+   </NavigationContainer>
+  )
+}
+
+export default AppNavigator
---
+++ src/screens/LoginScreen.tsx
@@ -0,0 +1,14 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+const LoginScreen = ({navigation}:any) => {
+  return (
+    <View>
+      <Text>LoginScreen</Text>
+    </View>
+  )
+}
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({})
